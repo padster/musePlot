fd2e4a0bfaf3f63f39f73d72266a4828d60b138d
padster <padsterpat@gmail.com>
2017-06-02 11:50:12 -0700

    Live scrolling graph working, hooked up to tbr for each sensor

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import matplotlib.pyplot as plt
 
 # pip3 install python-osc
 from pythonosc import dispatcher
@@ -7,28 +8,54 @@ from pythonosc import osc_server
 # See here for example:
 # http://developer.choosemuse.com/research-tools-example/grabbing-data-from-museio-a-few-simple-examples-of-muse-osc-servers#python
 
-lastQ = None
-lastT = None
-lastB = None
+import livegraph
 
-def process(q, t, b):
-    # TODO: 2x2 subplots, draw live connection strength to each.
-    # TODO: add t/b to numpy rolling buffer
-    # TODO: Scrolling mode: plot t/b buffer to animated matplotlib
+def cleanSubplots(r=2, c=2, pad=0.05):
+    f = plt.figure()
+    ax = []
+    at = 1
+    for i in range(r):
+        row = []
+        for j in range(c):
+            axHere = f.add_subplot(r, c, at)
+            axHere.get_xaxis().set_visible(False)
+            # axHere.get_yaxis().set_visible(False)
+            row.append(axHere)
+            at = at + 1
+        ax.append(row)
+    f.subplots_adjust(left=pad, right=1.0-pad, top=1.0-pad, bottom=pad, hspace=pad*4)
+    try:
+        plt.get_current_fig_manager().window.showMaximized()
+    except AttributeError:
+        pass # Can't maximize, sorry :(
+    return ax
+
+plots = []
+lastG = None
+lastB = None
+lastT = None
+
+def process(g, t, b):
+    for i in range(4):
+        r = i // 2
+        c = i % 2
+        plots[r][c].setGood(g[i] == 1)
+        plots[r][c].add(t[i]/b[i])
+    plt.pause(0.001)
     # TODO: Histogram mode: plot same but histogram of non-buffered version.
 
 def tryProcess():
-    global lastQ, lastT, lastB
-    if lastQ is None or lastT is None or lastB is None:
+    global lastG, lastB, lastT
+    if lastG is None or lastB is None or lastT is None:
         return
-    process(lastQ, lastT, lastB)
-    lastQ = None
-    lastT = None
+    process(lastG, lastB, lastT)
+    lastG = None
     lastB = None
+    lastT = None
 
-def qHandler(unused_addr, ch1, ch2, ch3, ch4):
-    global lastQ
-    lastQ = [ch1, ch2, ch3, ch4]
+def gHandler(unused_addr, ch1, ch2, ch3, ch4):
+    global lastG
+    lastG = [ch1, ch2, ch3, ch4]
     tryProcess()
 
 def bHandler(unused_addr, ch1, ch2, ch3, ch4):
@@ -54,11 +81,22 @@ if __name__ == "__main__":
                         help="The port to listen on")
     args = parser.parse_args()
 
+    # Listen to OSC channels we care about:
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/debug", print)
-    dispatcher.map("/muse/eeg/quantization", qHandler)
-    dispatcher.map("/muse/elements/theta_relative", tHandler)
+    dispatcher.map("/muse/elements/is_good", gHandler)
     dispatcher.map("/muse/elements/beta_relative", bHandler)
+    dispatcher.map("/muse/elements/theta_relative", tHandler)
+
+    # Build live graphs
+    axes = cleanSubplots()
+    for i in range(len(axes)):
+        plotRow = []
+        for j in range(len(axes[i])):
+            # TODO - titles for locations.
+            plotRow.append(livegraph.LiveGraph(axes[i][j]))
+        plots.append(plotRow)
+
 
     # server = osc_server.ThreadingOSCUDPServer(
     server = osc_server.BlockingOSCUDPServer(
